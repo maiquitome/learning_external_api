7770ae09d1c96456571c2040479d46e7b1958585
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-05-15 12:40:46 -0300

    :heavy_plus_sign: add httpoison dependency

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule MyApp.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tesla, "~> 1.4"},
-      {:hackney, "~> 1.17"}
+      {:hackney, "~> 1.17"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
